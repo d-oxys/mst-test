62fcc2a04a762553b2459859fcc7844f4014d199
D-oxys <sanuzumaki12@gmail.com>
2024-05-19 00:29:15 +0700

    first commit

--- src/components/Tables/TableTransaksi.tsx
+++ src/components/Tables/TableTransaksi.tsx
@@ -142,7 +142,13 @@ const TableThree = () => {
                 </td>
                 <td className='dark:border-strokedark border-b border-[#eee] px-4 py-5'>
                   <div className='flex items-center space-x-3.5'>
-                    <button onClick={() => router.push(`/forms/transaksi/${packageItem.kodeTransaksi}`)} className='hover:text-primary'>
+                    <button
+                      onClick={() => {
+                        router.push(`/forms/transaksi/${packageItem.kodeTransaksi}`);
+                        window.location.reload();
+                      }}
+                      className='hover:text-primary'
+                    >
                       <svg className='fill-current' width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
                         <path
                           d='M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z'
@@ -154,6 +160,7 @@ const TableThree = () => {
                         />
                       </svg>
                     </button>
+
                     <button onClick={() => handleDelete(packageItem.kodeTransaksi)} className='hover:text-primary'>
                       <svg className='fill-current' width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
                         <path
